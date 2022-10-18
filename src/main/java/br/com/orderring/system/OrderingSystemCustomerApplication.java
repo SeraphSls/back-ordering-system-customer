18b94eeffe45956a1576d9d46e0bdfb5f58834ad
welber <welberssl@primeit.com.br>
2022-10-18 15:53:23 -0300

    inclusão da classe modelo de request e criação do controller de customer

--- src/main/java/br/com/orderring/system/OrderingSystemCustomerApplication.java
+++ src/main/java/br/com/orderring/system/OrderingSystemCustomerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderingSystemApplication {
+public class OrderingSystemCustomerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderingSystemApplication.class, args);
+		SpringApplication.run(OrderingSystemCustomerApplication.class, args);
 	}
 
 }
---
+++ src/main/java/br/com/orderring/system/customer/controller/CustomerController.java
@@ -0,0 +1,18 @@
+package br.com.orderring.system.customer.controller;
+
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/customer")
+public class CustomerController {
+
+    @GetMapping(value = "/recoverCustomer")
+    public ResponseEntity<Object> getCustomer(){
+        return  new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
